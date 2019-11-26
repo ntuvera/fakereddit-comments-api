108297289da9b8aefccd5e0d3a01fe93c91927bd
Nate Tuvera <ntuvera@gmail.com>
2019-11-25 23:20:12 -0500

    Add swagger UI, butmissing two routes due to conflict

--- src/main/java/com/example/commentsapi/controller/CommentsApiController.java
+++ src/main/java/com/example/commentsapi/controller/CommentsApiController.java
@@ -2,19 +2,22 @@ package com.example.commentsapi.controller;
 
 import com.example.commentsapi.model.Comment;
 import com.example.commentsapi.service.CommentServiceImpl;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(value = "Comment Management System")
 public class CommentsApiController {
 
     @Autowired
     private CommentServiceImpl commentService;
 
-    @PostMapping("/{postId}")
-    public Comment createComment(@RequestBody Comment comment, @PathVariable int postId, @RequestHeader("userId") int userId, @RequestHeader("username") String username) {
-        return commentService.createComment(comment, postId, userId, username);
-    }
+    // TODO: reactivate when we find a way to go around swagger-ui.html
+//    @PostMapping("/{postId}")
+//    public Comment createComment(@RequestBody Comment comment, @PathVariable int postId, @RequestHeader("userId") int userId, @RequestHeader("username") String username) {
+//        return commentService.createComment(comment, postId, userId, username);
+//    }
 
     @GetMapping("/post/{postId}/comment")
     public Iterable<Comment> getCommentsByPostId(@PathVariable int postId) {
