15cab2b8367f31fd019d72997dca4dac1a3cc7ae
Steven Huang <steven.charles.huang@gmail.com>
2019-11-25 20:06:00 -0500

    added dummy test to test cobertura

---
+++ src/test/java/com/example/commentsapi/controller/CommentsAPIControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.commentsapi.controller;
+
+import com.example.commentsapi.service.CommentServiceImpl;
+import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+public class CommentsAPIControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    CommentServiceImpl commentsService;
+
+    @Test
+    public void dummyTest() throws Exception {
+
+    }
+}
